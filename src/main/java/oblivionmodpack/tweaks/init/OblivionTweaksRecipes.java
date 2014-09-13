119bbd68591930fbf31752b13baa5d8ec5f6e738
ViolentNinjaD <fiddlybilly@gmail.com>
2014-09-13 10:11:19 +0100

    APIs and prep for recipes :-)

---
+++ src/main/java/oblivionmodpack/tweaks/init/OblivionTweaksRecipes.java
@@ -0,0 +1,19 @@
+package oblivionmodpack.tweaks.init;
+/*@author ViolentNinjaD
+
+  Licensed under LGPL V3
+
+*/
+public class OblivionTweaksRecipes
+{
+    public static void init()
+    {
+
+    }
+
+    public static void craftingRecipes()
+    {
+
+    }
+
+}
